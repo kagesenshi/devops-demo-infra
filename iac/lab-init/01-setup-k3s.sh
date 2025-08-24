69f76a9f46dea4ac105de664f98e11ff4d3ce87c
Izhar Firdaus <kagesenshi.87@gmail.com>
2025-08-24 23:01:13 +0800

    initial import

---
+++ iac/lab-init/01-setup-k3s.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+#ansible-playbook 01-setup-k3s.yml --ask-vault-password -i inventory
+
+for I in `ls fetch`;do
+    cat fetch/$I/etc/rancher/k3s/k3s.yaml|sed "s|127.0.0.1|$I|g" > kubeconfig
+    echo "kubeconfig saved to $PWD/kubeconfig"
+done
